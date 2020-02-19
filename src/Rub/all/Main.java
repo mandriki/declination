8954136c38bb809d28b929b47dcea61b2d6928b9
Sergey Kuznitsou <50965920+mandriki@users.noreply.github.com>
2020-02-20 01:12:25 +0300

    Занятие 2 : Условные операторы. Функции. Задачи

---
+++ src/Rub/all/Main.java
@@ -0,0 +1,46 @@
+package Rub.all;
+import java.lang.invoke.SwitchPoint;
+import  java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Программа склоние \"Рубль\"");
+        System.out.print("Введите целое число и нажмите enter: ");
+        int i = sc.nextInt();
+        switch(i % 100) {
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+                System.out.println(i + " Рублей");
+                break;
+            default:
+        switch (i % 10) {
+            case 0:
+                System.out.println(i + " Рублей");
+                break;
+            case 1:
+                System.out.println(i + " Рубль");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println(i + " Рубля");
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                System.out.println(i + " Рублей");
+             }
+        }
+
+    }
+}
